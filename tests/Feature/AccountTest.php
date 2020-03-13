a9b363e9ffde15d511077d0cb26fecb52502bc46
Caio Vinicius <caio@vncius.com>
2020-03-13 00:38:32 -0300

    Create a unit test to register new account

---
+++ tests/Feature/AccountTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Account\Contracts\AccountCreatable;
+use Faker\Factory;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class AccountTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test register new account service
+     *
+     * @return void
+     */
+    public function testRegister()
+    {
+        Notification::fake();
+
+        $service = app()->make(AccountCreatable::class);
+        $faker = Factory::create();
+        $account = $service->register($faker->name, $faker->email, $faker->password, $faker->company);
+
+        $this->assertNotNull($account);
+        $this->assertInstanceOf(\App\User::class, $account);
+        $this->assertNotNull($account->workspaces()->first());
+        $this->assertTrue(Str::isUuid($account->public_id));
+        $this->assertTrue(Str::isUuid($account->workspaces()->first()->public_id));
+        $this->assertNull($account->email_verified_at);
+
+    }
+}
